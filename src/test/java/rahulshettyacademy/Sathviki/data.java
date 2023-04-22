d0fd4c0de9c5cb6dbb6b3e2181dc69775c96db02
Shashi <shashidhar1306@gmail.com>
2023-04-22 06:32:27 +0530

    second time new code is pushed into GITHUB

--- src/test/java/rahulshettyacademy/Sathviki/data.java
+++ src/test/java/rahulshettyacademy/Sathviki/data.java
@@ -11,4 +11,10 @@ public class data {
 		System.out.println("I am Automation tester");
 		
 	}
+	public void setData()  {
+		System.out.println("Good Morning");
+		System.out.println("Today is RAMZAAN");
+		System.out.println("Keep Smiling, Have a Great Day");
+		
+	}
 }
