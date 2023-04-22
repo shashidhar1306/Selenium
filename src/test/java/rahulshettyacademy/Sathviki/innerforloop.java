bf99f50936338362bce0d8284be909a4da4f5321
Shashi <shashidhar1306@gmail.com>
2023-04-22 07:22:20 +0530

    New Innerloop file committed into GITHUB

---
+++ src/test/java/rahulshettyacademy/Sathviki/innerforloop.java
@@ -0,0 +1,28 @@
+package rahulshettyacademy.Sathviki;
+
+
+
+public class innerforloop {
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+		
+		for(int a=0;a<4;a++)
+		{
+			//System.out.println("Outerloop start");
+		
+		
+			for(int d=1;d<=a;d++)
+				
+			{
+				System.out.print(d);
+				System.out.print("\t");
+			
+			}
+			System.out.println(" ");
+		
+		
+		}
+}
+}
